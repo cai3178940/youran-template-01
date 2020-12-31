c8bfc7ab486d9804ffa1f398b5e07eaf1d26de86
蔡斌冰 <caibinbing@foxmail.com>
2020-12-31 16:32:09 +0800

    fix: 微调

--- src/main/build/build.js
+++ src/main/build/build.js
@@ -17,24 +17,32 @@ function getRelativePath(targetDir, baseDir) {
 }
 
 System.out.println('开始构建template.json文件')
+// 解析初始template.json
 var obj = JSON.parse(FileUtils.readFileToString(new File('src/main/build/template.json'), 'utf-8'))
+// 读取remark
 obj.remark = FileUtils.readFileToString(new File('src/main/build/remark.md'), 'utf-8') + ''
-
+// 初始化模板文件数组
 obj.templateFiles = []
+// freemarker的基础目录
 var baseDir = new File('src/main/resources/ftl')
+// 获取所有子目录
 var dirs = FileUtils.listFilesAndDirs(baseDir, FileFilterUtils.directoryFileFilter(), FileFilterUtils.trueFileFilter())
 for (var i = 0; i < dirs.size(); i++) {
     var dir = dirs.get(i)
     var typeFile = new File(dir, '__type.json')
     if (typeFile.exists()) {
+        // 解析__type.json内的数据
         var types = JSON.parse(FileUtils.readFileToString(typeFile, 'utf-8'))
         for (var j = 0; j < types.length; j++) {
             var type = types[j]
+            // 将目录字段补充上
             type.fileDir = '/' + getRelativePath(dir, baseDir)
             obj.templateFiles.push(type)
         }
     }
 }
+// 生成整个完整的template.json文件内容
 var output = JSON.stringify(obj, null, 2);
 System.out.println(output)
+// 写入template.json文件
 FileUtils.writeStringToFile(new File('src/main/resources/template.json'), output, 'utf-8')
